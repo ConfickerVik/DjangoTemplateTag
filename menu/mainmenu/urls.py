4ae4dadb645b7df02507243a1dacaa40015a0396
ConfickerVik <viktor.kf.98@mail.ru>
2024-04-12 15:51:16 +0800

    clear urls and views

--- menu/mainmenu/urls.py
+++ menu/mainmenu/urls.py
@@ -4,9 +4,5 @@ from .views import index
 
 urlpatterns = [
     path('menu/', index, name="index"),
-    path('<path:path>/', index, name="draw_menu")
-    # re_path(r'^(\w+)/$', index, name='draw_menu'),
-    # re_path(r'^(\w+)/(\w+)/$', index, name='index'),
-    # re_path(r'^(\w+)/(\w+)/(\w+)/$', index, name='index'),
-    # re_path(r'^(\w+)/(\w+)/(\w+)/(\w+)/$', index, name='index')
+    path('<path:path>/', index, name="draw_menu"),
 ]
--- menu/mainmenu/views.py
+++ menu/mainmenu/views.py
@@ -1,11 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request, path=""):
     return render(request, template_name="index.html", )
-
-
-# def draw_menu(request, path):
-#     splitted_path = path.split('/')
-#     return redirect("/" + splitted_path[-1] + "/")
