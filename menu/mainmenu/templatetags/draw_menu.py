db9557e5f33cf86e70dee062902783c94d0a88e4
ConfickerVik <viktor.kf.98@mail.ru>
2024-04-12 15:29:07 +0800

    rework draw tree menu

--- menu/mainmenu/templatetags/draw_menu.py
+++ menu/mainmenu/templatetags/draw_menu.py
@@ -16,12 +16,17 @@ def draw_menu(context, menu_name):
             if item.link_url in menu_html:
                 continue
             menu_html += '<li>'
-            if item.link_url.replace("/", "") in context["request"].get_full_path()[1:].replace("/", ""):
-                menu_html += f'<a class="active" href="{item.link_url}">{item.title}</a>'
+
+            if item.menu_items.exists():
+                if item.link_url.replace("/", "") in context["request"].get_full_path()[1:].replace("/", ""):
+                    menu_html += f'<details open><summary><a href="{item.link_url}">{item.title}</a></summary>'
+                else:
+                    menu_html += f'<details><summary><a href="{item.link_url}">{item.title}</a></summary>'
+                menu_html += render_menu(item.menu_items.all(), class_ul="submenu")
+                menu_html += '</details>'
             else:
                 menu_html += f'<a href="{item.link_url}">{item.title}</a>'
-            if item.menu_items.exists():
-                menu_html += render_menu(item.menu_items.all(), class_ul="submenu")
+
             menu_html += '</li>'
         menu_html += '</ul>'
 
